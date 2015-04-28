926dc2474b270f6952d60869e2bc8a2f303354d0
Brisk <briskgreen@163.com>
2015-04-28 13:13:32 +0800

    ini存储数据结构

---
+++ ini_data.c
@@ -0,0 +1,315 @@
+#include "ini_data.h"
+
+INI_TABLE *ini_table_new(const char *name)
+{
+	INI_TABLE *table;
+
+	table=malloc(sizeof(INI_TABLE));
+	if(table == NULL)
+	{
+		ini_errno=INI_TABLE_NO_MEM;
+		return NULL;
+	}
+
+	table->name=strdup(name);
+	if(table->name == NULL)
+	{
+		ini_errno=INI_TABLE_NAME;
+		free(table);
+		return NULL;
+	}
+	table->node=NULL;
+	table->next=NULL;
+
+	return table;
+}
+
+INI *ini_new(void)
+{
+	INI *ini;
+	int i;
+
+	ini=malloc(sizeof(INI));
+	if(ini == NULL)
+	{
+		ini_errno=INI_NO_MEM;
+		return NULL;
+	}
+
+	ini->count=0;
+	ini->table=NULL;
+
+	return ini;
+}
+
+INI_NODE *ini_node_new(const char *key,const char *value)
+{
+	INI_NODE *node;
+
+	node=malloc(sizeof(INI_NODE));
+	if(node == NULL)
+	{
+		ini_errno=INI_NODE_NO_MEM;
+		return NULL;
+	}
+
+	node->key=strdup(key);
+	if(!node->key)
+	{
+		ini_errno=INI_NODE_KEY;
+		return NULL;
+	}
+	node->value=strdup(value);
+	if(!node->value)
+	{
+		ini_errno=INI_NODE_VALUE;
+		return NULL;
+	}
+	node->next=NULL;
+
+	return node;
+}
+
+int ini_node_add(INI_TABLE *table,const char *name,INI_NODE *node)
+{
+	while(table)
+	{
+		if(strcmp(table->name,name) == 0)
+			break;
+
+		table=table->next;
+	}
+
+	if(table == NULL)
+		return INI_NODE_NOT_FOUND;
+
+	if(table->node == NULL || table->node->key == NULL)
+		table->node=node;
+	else
+	{
+		INI_NODE *temp;
+
+		temp=table->node;
+		while(temp->next)
+			temp=temp->next;
+
+		temp->next=node;
+	}
+
+	++table->count;
+	return INI_OK;
+}
+
+int ini_table_has_key(INI_TABLE *table,const char *name,const char *key)
+{
+	INI_NODE *node;
+
+	while(table)
+	{
+		if(strcmp(table->name,name) == 0)
+			break;
+
+		table=table->next;
+	}
+
+	if(table == NULL) return INI_NO;
+	node=table->node;
+	if(!node)
+		return INI_NO;
+	while(node->next)
+	{
+		if(strcmp(node->key,key) == 0)
+			return INI_OK;
+
+		node=node->next;
+	}
+
+	return INI_NO;
+}
+
+int ini_has_table(INI *ini,const char *name)
+{
+	INI_TABLE *table=ini->table;
+
+	while(table)
+	{
+		if(strcmp(table->name,name) == 0)
+			return INI_OK;
+
+		table=table->next;
+	}
+
+	return INI_NO;
+}
+
+void ini_node_del_with_key(INI_TABLE *table,const char *name,const char *key)
+{
+	INI_NODE *node;
+	INI_NODE *temp;
+
+	while(table)
+	{
+		if(strcmp(table->name,name) == 0)
+			break;
+
+		table=table->next;
+	}
+
+	if(!table) return;
+	node=table->node;
+	if(!node) return;
+	if(node->next == NULL || strcmp(node->key,key) == 0)
+	{
+		temp=node;
+		table->node=node->next;
+		--table->count;
+		free(temp->key);
+		free(temp->value);
+		free(temp);
+		return;
+	}
+
+	while(node->next)
+	{
+		if(strcmp(node->next->key,key) == 0)
+		{
+			temp=node->next;
+			node->next=node->next->next;
+			--table->count;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			return;
+		}
+
+		node=node->next;
+	}
+}
+
+void ini_table_del_with_name(INI *ini,const char *name)
+{
+	INI_TABLE *table;
+	INI_TABLE *temp;
+
+	if(!ini) return;
+	table=ini->table;
+	if(!table) return;
+	if(table->next == NULL || strcmp(table->name,name) == 0)
+	{
+		temp=table;
+		ini->table=table->next;
+		--ini->count;
+		ini_node_free(temp->node);
+		free(temp);
+		return;
+	}
+
+	while(table->next)
+	{
+		if(strcmp(table->next->name,name) == 0)
+		{
+			temp=table->next;
+			table->next=table->next->next;
+			--ini->count;
+			ini_node_free(temp->node);
+			free(temp);
+			return;
+		}
+
+		table=table->next;
+	}
+}
+
+int ini_table_add(INI *ini,const char *name)
+{
+	INI_TABLE *table;
+	INI_TABLE *table_node;
+
+	table_node=ini_table_new(name);
+	if(!table_node) return ini_errno;
+	if(ini->table == NULL)
+		ini->table=table_node;
+	else
+	{
+		table=ini->table;
+
+		while(table->next)
+			table=table->next;
+
+		table->next=table_node;
+	}
+
+	++ini->count;
+	return INI_OK;
+}
+
+void ini_node_free(INI_NODE *node)
+{
+	INI_NODE *temp;
+
+	if(node)
+	{
+		while(node)
+		{
+			temp=node;
+			node=node->next;
+
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+}
+
+void ini_table_free(INI_TABLE *table)
+{
+	INI_TABLE *temp;
+
+	if(table)
+	{
+		while(table)
+		{
+			temp=table;
+			table=table->next;
+
+			free(temp->name);
+			ini_node_free(temp->node);
+			free(temp);
+		}
+	}
+}
+
+void ini_free(INI *ini)
+{
+	if(ini)
+	{
+		if(ini->table)
+			ini_table_free(ini->table);
+
+		free(ini);
+	}
+}
+
+char *ini_table_get_key_value(INI_TABLE *table,const char *name,const char *key)
+{
+	INI_NODE *node;
+
+	while(table)
+	{
+		if(strcmp(table->name,name) == 0)
+			break;
+
+		table=table->next;
+	}
+	if(table == NULL) return NULL;
+	node=table->node;
+	while(node)
+	{
+		if(strcmp(node->key,key) == 0)
+			return node->value;
+
+		node=node->next;
+	}
+
+	return NULL;
+}
---
+++ ini_data.h
@@ -0,0 +1,88 @@
+#ifndef _INI_DATA_H
+#define _INI_DATA_H
+
+#include "ini_error.h"
+#include <string.h>
+
+//节点数据
+typedef struct ini_node
+{
+	char *key;
+	char *value;
+
+	struct ini_node *next;
+}INI_NODE;
+
+//表
+typedef struct ini_table
+{
+	char *name; //表名称
+	int count; //表内节点数
+	INI_NODE *node; //表内节点
+
+	struct ini_table *next; //下一个表
+}INI_TABLE;
+
+typedef struct
+{
+	int count; //表数量
+	INI_TABLE *table; //表
+}INI;
+
+/*
+ * 创建一个表
+ * name 该表的名称
+ * 创建成功时返回一个INI_TABLE指针
+ * 出错时返回NULL并设置错误代码
+ */
+INI_TABLE *ini_table_new(const char *name);
+
+/*
+ * 创建一个ini配置文件结构
+ * 成功时返回一个INI结构指针
+ * 出错时返回NULL并设置错误代码
+ */
+INI *ini_new(void);
+
+//创建新节点
+INI_NODE *ini_node_new(const char *key,const char *value);
+
+/*
+ * 向table为name的表中添加一个结点
+ * 成功时返回INI_OK
+ * 失败时返回错误代码
+ */
+int ini_node_add(INI_TABLE *table,const char *name,INI_NODE *node);
+
+/*
+ * 从table为name的表中查询是否有键为key的节点
+ * 查询成功时返回INI_OK
+ * 没有查询到结果时返回INI_NO
+ */
+int ini_table_has_key(INI_TABLE *table,const char *name,const char *key);
+
+//查询表
+int ini_has_table(INI *ini,const char *name);
+
+//从table为name的表中删除键为key的节点
+void ini_node_del_with_key(INI_TABLE *table,const char *name,const char *key);
+
+//删除一个表
+void ini_table_del_with_name(INI *ini,const char *name);
+
+//添加一个表
+int ini_table_add(INI *ini,const char *name);
+
+//释放节点资源
+void ini_node_free(INI_NODE *node);
+
+//释放表资源
+void ini_table_free(INI_TABLE *table);
+
+//释放资源
+void ini_free(INI *ini);
+
+//获取一个表name中节点key的值
+char *ini_table_get_key_value(INI_TABLE *table,const char *name,const char *key);
+
+#endif
